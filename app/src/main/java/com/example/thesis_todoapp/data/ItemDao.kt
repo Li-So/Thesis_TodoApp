a0bfc33b587b697c42852a93becfd79daf7e7c1e
Li-So <lin.sonnerhed@gmail.com>
2024-03-08 17:26:17 +0100

    Added the ItemDao

---
+++ app/src/main/java/com/example/thesis_todoapp/data/ItemDao.kt
@@ -0,0 +1,27 @@
+package com.example.thesis_todoapp.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItemDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(todo: TodoItem)
+
+    @Update
+    suspend fun update(todo: TodoItem)
+
+    @Delete
+    suspend fun delete(todo: TodoItem)
+
+    @Query("SELECT * FROM todoItems WHERE id = :id")
+    fun getTodoItem(id: Int): Flow<TodoItem>
+
+    @Query("SELECT * FROM todoItems ORDER BY label ASC")
+    fun getAllTodoItems(): Flow<List<TodoItem>>
+}
--- app/src/main/java/com/example/thesis_todoapp/data/TodoItem.kt
+++ app/src/main/java/com/example/thesis_todoapp/data/TodoItem.kt
@@ -1,5 +1,13 @@
 package com.example.thesis_todoapp.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 
-data class TodoItem(val id:Int, val label: String, val dateChecked: Date)
+@Entity(tableName = "todoItems")
+data class TodoItem(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val label: String,
+    val dateChecked: Date
+)
