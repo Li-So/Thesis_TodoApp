4a997b1272dbd08105c2d08fc344bf89604405f7
Li-So <lin.sonnerhed@gmail.com>
2024-03-13 23:38:54 +0100

    Removed imports

--- app/src/main/java/com/example/thesis_todoapp/ui/components/DismissBackground.kt
+++ app/src/main/java/com/example/thesis_todoapp/ui/components/DismissBackground.kt
@@ -3,7 +3,6 @@ package com.example.thesis_todoapp.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
--- app/src/main/java/com/example/thesis_todoapp/ui/components/TodoTaskItemList.kt
+++ app/src/main/java/com/example/thesis_todoapp/ui/components/TodoTaskItemList.kt
@@ -25,14 +25,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.thesis_todoapp.R
 import com.example.thesis_todoapp.data.TodoItem
-import com.example.thesis_todoapp.ui.theme.ThesisTodoAppTheme
-import java.util.Date
 
 @Composable
 fun TodoTaskItemList(
